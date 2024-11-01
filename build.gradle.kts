4f5b474056163acb786d8201ca286e9865182955
nicolasfanin <faninnicolas@gmail.com>
2024-11-01 16:30:28 -0300

    navigations

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id(libs.plugins.navigation.safe.args.get().pluginId)
 }
